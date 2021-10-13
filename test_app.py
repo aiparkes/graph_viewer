84917a049545070704de84802a9e593b3daca870
Amy Parkes <amy@arcsilea.com>
2021-10-13 18:33:51 +0100

    new app

--- test_app.py
+++ test_app.py
@@ -6,9 +6,10 @@ from plotly.subplots import make_subplots
 from layout import _max_width_
 _max_width_()
 
+st.header('CII Tool (Bulk Carrier Prototype)')
 
 ref = pd.DataFrame(columns=['A','B','Reference line','D','E'])
-yearly_decrease = {2021:2,2022:3,2023:5,2024:7,2025:9,2026:11,2027:14,2028:17,2029:20,2030:23}
+
 bulk_ref_line = lambda DWT : 4745*(DWT**-0.622)
 boundaries = {'A':0.86,'B':0.94,'D':1.06,'E':1.18}
 colours = {'E':'red','D':'orange','Reference line':'black','B':'lightgreen','A':'darkgreen'}
@@ -18,9 +19,14 @@ col1, col2, col3 = st.columns([0.75,0.125,0.125])
 vessel_type = col2.selectbox('Vessel Type',('Bulk Carrier',''))
 input_AER = col2.number_input('Input AER', value=1.5)
 input_DWT = col2.number_input('Input Deadweight', value=260000)
+perc_decr = col2.number_input('Percentage Decrease Post 2026', value=3.0, format="%.1f")
+
+yearly_decrease = {2021:2,2022:3,2023:5,2024:7,2025:9,2026:11,2027:11+perc_decr,2028:11+perc_decr*2,2029:11+perc_decr*3,2030:11+perc_decr*4}
+
 
 if input_DWT > 279000:
     input_DWT = 279000
+ref['Percentage Decrease'] = yearly_decrease.values()
 ref['Reference line'] = [bulk_ref_line(input_DWT)*(1-perc/100) for perc in yearly_decrease.values()]
 ref['A'] = ref['Reference line']*boundaries['A']
 ref['B'] = ref['Reference line']*boundaries['B']
@@ -86,7 +92,6 @@ fig['layout']['yaxis2']['visible']=False
 fig['layout']['xaxis2']['visible']=False
 
 fig.update_layout(
-	title_text = 'CII tool',
 	showlegend = True,
 	width=1100,
 	height=500,
@@ -96,6 +101,8 @@ fig.update_layout(
 col1.plotly_chart(fig, use_container_width = True)
 
 
-col11, col12 = st.columns([0.1,0.8])
+col11, col12, col13 = st.columns([0.1,0.5,0.4])
 
-col12.dataframe(ref[['A','B','Reference line','D','E']].transpose().style.format("{:.4}"))
+col12.dataframe(ref[['Percentage Decrease','A','B','Reference line','D','E']].transpose().style.format("{:.4}"))
+#yearly_decrease = pd.DataFrame({2021:[2],2022:[3],2023:[5],2024:[7],2025:[9],2026:[11],2027:[11+perc_decr],2028:[11+perc_decr*2],2029:[11+perc_decr*3],2030:[11+perc_decr*4]})
+#col13.write(yearly_decrease)
